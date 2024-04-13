c22b4545bdb4db9187abb055f390e72471c7b2e2
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-13 12:44:48 +0300

    Update SoulsCounterComponent

--- Source/LudumDare55/Components/SoulsCounterComponent.cpp
+++ Source/LudumDare55/Components/SoulsCounterComponent.cpp
@@ -2,3 +2,33 @@
 
 
 #include "SoulsCounterComponent.h"
+
+USoulsCounterComponent::USoulsCounterComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void USoulsCounterComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+bool USoulsCounterComponent::AddSouls(const int32 Amount)
+{
+	if (Amount <= 0)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool USoulsCounterComponent::SpendSouls(const int32 Amount)
+{
+	if (Amount <= 0)
+	{
+		return false;
+	}
+
+	return true;
+}
--- Source/LudumDare55/Components/SoulsCounterComponent.h
+++ Source/LudumDare55/Components/SoulsCounterComponent.h
@@ -3,13 +3,41 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/SimpleResourceComponent.h"
 
 #include "SoulsCounterComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnSoulsAddedDynamicSignature,
+                                               USoulsCounterComponent*, Component,
+                                               int32, NewSouls,
+                                               int32, DeltaSouls);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnSoulsSpentDynamicSignature,
+                                               USoulsCounterComponent*, Component,
+                                               int32, NewSouls,
+                                               int32, DeltaSouls);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class LUDUMDARE55_API USoulsCounterComponent : public USimpleResourceComponent
+class LUDUMDARE55_API USoulsCounterComponent : public UActorComponent
 {
 	GENERATED_BODY()
+
+private:
+	USoulsCounterComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	bool AddSouls(const int32 Amount);
+
+	UFUNCTION(BlueprintCallable)
+	bool SpendSouls(const int32 Amount);
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetSouls() const { return Souls; }
+
+private:
+	UPROPERTY(BlueprintGetter=GetSouls)
+	int32 Souls = 0;
 };

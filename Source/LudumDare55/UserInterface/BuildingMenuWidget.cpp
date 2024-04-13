8ad7ac28ca470d91238acdc6368b52a0fae5cf57
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-13 23:30:07 +0300

    Create building menu widget

---
+++ Source/LudumDare55/UserInterface/BuildingMenuWidget.cpp
@@ -0,0 +1,86 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "BuildingMenuWidget.h"
+
+#include "ButtonWidget.h"
+#include "TrickyGameModeLibrary.h"
+#include "Components/HorizontalBox.h"
+#include "Components/Spacer.h"
+#include "Kismet/GameplayStatics.h"
+
+void UBuildingMenuWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (Button_Quit)
+	{
+		Button_Quit->OnButtonClicked.AddUniqueDynamic(this, &UBuildingMenuWidget::HandleButtonClicked);
+	}
+}
+
+void UBuildingMenuWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	if (!HorizontalBox_Buttons)
+	{
+		return;
+	}
+
+	HorizontalBox_Buttons->ClearChildren();
+	Buttons.Empty();
+
+	if (Buttons.IsEmpty())
+	{
+		return;
+	}
+
+	for (auto Button : ButtonClasses)
+	{
+		if (!Button)
+		{
+			continue;
+		}
+
+		UButtonWidget* NewButton = CreateWidget<UButtonWidget>(this, Button);
+		USpacer* Spacer = NewObject<USpacer>(this, USpacer::StaticClass());
+		Spacer->SetSize(FVector2D{0.f, SpaceBetweenButtons});
+		HorizontalBox_Buttons->AddChildToHorizontalBox(NewButton);
+		HorizontalBox_Buttons->AddChildToHorizontalBox(Spacer);
+		Buttons.Add(NewButton);
+	}
+}
+
+void UBuildingMenuWidget::ShowButtons()
+{
+	SetButtonsVisibility(true);
+}
+
+
+void UBuildingMenuWidget::HandleButtonClicked(UButtonWidget* ButtonWidget)
+{
+	SetButtonsVisibility(false);
+	UTrickyGameModeLibrary::SetMouseEventsEnabled(this, true);
+}
+
+void UBuildingMenuWidget::SetButtonsVisibility(const bool bIsVisible)
+{
+	ESlateVisibility NewVisibility = bIsVisible ? ESlateVisibility::Visible : ESlateVisibility::Hidden;
+	Button_Quit->SetVisibility(NewVisibility);
+
+	if (Buttons.IsEmpty())
+	{
+		return;
+	}
+
+	for (auto Button : Buttons)
+	{
+		if (!Button)
+		{
+			continue;
+		}
+
+		Button->SetVisibility(NewVisibility);
+	}
+}
---
+++ Source/LudumDare55/UserInterface/BuildingMenuWidget.h
@@ -0,0 +1,49 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseUserWidget.h"
+
+#include "BuildingMenuWidget.generated.h"
+
+class UHorizontalBox;
+class UButtonWidget;
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE55_API UBuildingMenuWidget : public UBaseUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeOnInitialized() override;
+
+	virtual void NativePreConstruct() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void ShowButtons();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UHorizontalBox> HorizontalBox_Buttons = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(BindWidget))
+	TObjectPtr<UButtonWidget> Button_Quit = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<TSubclassOf<UButtonWidget>> ButtonClasses = {};
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<TObjectPtr<UButtonWidget>> Buttons;
+	
+	UPROPERTY(BlueprintReadOnly)
+	float SpaceBetweenButtons = 15.f;
+
+	UFUNCTION()
+	void HandleButtonClicked(UButtonWidget* ButtonWidget);
+
+	void SetButtonsVisibility(const bool bIsVisible);
+};

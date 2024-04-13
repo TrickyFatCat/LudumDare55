6104e6c42ebe924f11fd74250edbf6d319fc406f
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-13 12:47:00 +0300

    Create PlayerPawnActor

---
+++ Source/LudumDare55/Gameplay/PlayerPawn.cpp
@@ -0,0 +1,30 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "PlayerPawn.h"
+
+#include "LudumDare55/Components/SoulsCounterComponent.h"
+
+
+APlayerPawn::APlayerPawn()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	SoulsCounterComponent = CreateDefaultSubobject<USoulsCounterComponent>("SoulsCounter");
+}
+
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+

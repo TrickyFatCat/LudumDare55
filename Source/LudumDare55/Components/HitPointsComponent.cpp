484e21e20d6273122eb61eb128fc2254074e478b
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-13 12:22:00 +0300

    Create HitPointsComponent

---
+++ Source/LudumDare55/Components/HitPointsComponent.cpp
@@ -0,0 +1,25 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "HitPointsComponent.h"
+
+UHitPointsComponent::UHitPointsComponent()
+{
+	bWantsInitializeComponent = true;
+}
+
+void UHitPointsComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	GetOwner()->OnTakeAnyDamage.AddUniqueDynamic(this, &UHitPointsComponent::HandleAnyDamageTaken);
+}
+
+void UHitPointsComponent::HandleAnyDamageTaken(AActor* DamagedActor,
+                                               float Damage,
+                                               const UDamageType* DamageType,
+                                               AController* InstigatedBy,
+                                               AActor* DamageCauser)
+{
+	DecreaseValue(Damage);
+}
---
+++ Source/LudumDare55/Components/HitPointsComponent.h
@@ -0,0 +1,26 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ResourceComponent.h"
+#include "HitPointsComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE55_API UHitPointsComponent : public UResourceComponent
+{
+	GENERATED_BODY()
+
+private:
+	UHitPointsComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+	UFUNCTION()
+	void HandleAnyDamageTaken(AActor* DamagedActor,
+	                          float Damage,
+	                          const UDamageType* DamageType,
+	                          AController* InstigatedBy, AActor* DamageCauser);
+};

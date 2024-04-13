99349656e6adff3bfbf1fd59bc31948c4300da9e
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-13 17:28:39 +0300

    Create building actor

---
+++ Source/LudumDare55/Gameplay/Building.cpp
@@ -0,0 +1,21 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "Building.h"
+
+
+ABuilding::ABuilding()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABuilding::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABuilding::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+

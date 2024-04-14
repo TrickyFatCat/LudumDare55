3eec7d49144bce5fac63b8fc2ec8c1c98c68e2d2
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-14 07:36:49 +0300

    Add forgotten header

--- Source/LudumDare55/UserInterface/BuildingButtonWidget.h
+++ Source/LudumDare55/UserInterface/BuildingButtonWidget.h
@@ -6,6 +6,7 @@
 #include "ButtonWidget.h"
 #include "BuildingButtonWidget.generated.h"
 
+class ABuilding;
 /**
  * 
  */
@@ -13,4 +14,11 @@ UCLASS()
 class LUDUMDARE55_API UBuildingButtonWidget : public UButtonWidget
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetOwningBuilding(ABuilding* Building) { OwningBuilding = Building; }
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ABuilding> OwningBuilding = nullptr;
 };

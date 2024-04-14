9461d9c587b64ec429085c805128eba9895809c3
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-14 21:20:58 +0300

    Create EnemySoulComponent

---
+++ Source/LudumDare55/Components/EnemySoulComponent.cpp
@@ -0,0 +1,33 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "EnemySoulComponent.h"
+
+#include "SoulsCounterComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+UEnemySoulComponent::UEnemySoulComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UEnemySoulComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APawn* Pawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	SoulsCounterComponent = Pawn->GetComponentByClass<USoulsCounterComponent>();
+}
+
+bool UEnemySoulComponent::IncreaseSouls()
+{
+	if (!SoulsCounterComponent)
+	{
+		return false;
+	}
+
+	return SoulsCounterComponent->AddSouls(SoulsAmount);
+}
+

77f27c744ab19bee987cb537dcc6fb352654c2d7
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-14 12:41:04 +0300

    Create MinionLifeTimerComponent

---
+++ Source/LudumDare55/Components/MinionLifeTimeComponent.cpp
@@ -0,0 +1,30 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "MinionLifeTimeComponent.h"
+
+#include "MinionsCounterComponent.h"
+
+
+UMinionLifeTimeComponent::UMinionLifeTimeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UMinionLifeTimeComponent::StopTimer()
+{
+	GetWorld()->GetTimerManager().ClearTimer(LifeTimerHandle);
+	OnLifeExpired.Broadcast();
+}
+
+void UMinionLifeTimeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(LifeTimerHandle, this, &UMinionLifeTimeComponent::HandleLifeExpired, LifeDuration);
+}
+
+void UMinionLifeTimeComponent::HandleLifeExpired()
+{
+	OnLifeExpired.Broadcast();
+}

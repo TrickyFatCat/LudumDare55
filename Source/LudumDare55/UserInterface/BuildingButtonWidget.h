ae6a66f87f6e04fe3e8e658c7c68cb6ca664d8bd
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-14 07:36:02 +0300

    Create building button widget

---
+++ Source/LudumDare55/UserInterface/BuildingButtonWidget.h
@@ -0,0 +1,16 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ButtonWidget.h"
+#include "BuildingButtonWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE55_API UBuildingButtonWidget : public UButtonWidget
+{
+	GENERATED_BODY()
+};

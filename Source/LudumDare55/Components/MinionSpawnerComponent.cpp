1002695ef9e37fb202d1569e4a00fe2f2864ec57
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-14 08:42:57 +0300

    Create MinionSpawnerComponent

---
+++ Source/LudumDare55/Components/MinionSpawnerComponent.cpp
@@ -0,0 +1,26 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "MinionSpawnerComponent.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "LudumDare55/Characters/Minion.h"
+
+
+UMinionSpawnerComponent::UMinionSpawnerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+bool UMinionSpawnerComponent::SpawnMinion(TSubclassOf<AMinion> MinionClass)
+{
+	if (!MinionClass)
+	{
+		return false;
+	}
+
+	AMinion* Minion = GetWorld()->SpawnActorDeferred<AMinion>(MinionClass, GetOwner()->GetTransform());
+	Minion->FinishSpawning(GetOwner()->GetTransform());
+	OnMinionSpawned.Broadcast(this, Minion);
+	return true;
+}

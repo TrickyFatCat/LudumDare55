4bc9f4024ee0f9cb08c3751f1f39840de69ac470
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-14 06:54:05 +0300

    Create Minion actor class

---
+++ Source/LudumDare55/Characters/Minion.cpp
@@ -0,0 +1,25 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "Minion.h"
+
+#include "LudumDare55/Components/HitPointsComponent.h"
+
+
+AMinion::AMinion()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	HitPointsComponent = CreateDefaultSubobject<UHitPointsComponent>("HitPoints");
+}
+
+void AMinion::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMinion::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/LudumDare55/Characters/Minion.h
@@ -0,0 +1,30 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Minion.generated.h"
+
+class UHitPointsComponent;
+
+UCLASS()
+class LUDUMDARE55_API AMinion : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AMinion();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UHitPointsComponent> HitPointsComponent = nullptr;
+};

c85622e85c8ef9f0d8c91b38bca02f723b991177
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-14 16:37:12 +0300

    Create EnemySpawnManager

---
+++ Source/LudumDare55/SpawnSystem/EnemySpawnManager.cpp
@@ -0,0 +1,119 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "EnemySpawnManager.h"
+
+#include "EnemySpawnPoint.h"
+#include "Kismet/GameplayStatics.h"
+#include "LudumDare55/Characters/Enemy.h"
+
+
+AEnemySpawnManager::AEnemySpawnManager()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AEnemySpawnManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	EnemySpawnPoints.Reset();
+	TArray<AActor*> OutActors;
+	UGameplayStatics::GetAllActorsOfClass(this, AEnemySpawnPoint::StaticClass(), OutActors);
+
+	for (const auto Actor : OutActors)
+	{
+		EnemySpawnPoints.Add(Cast<AEnemySpawnPoint>(Actor));
+	}
+}
+
+void AEnemySpawnManager::RandomizeEnemies()
+{
+	if (bIsRandomSpawn)
+	{
+		const int32 LastIndex = EnemySpawnPoints.Num() - 1;
+
+		for (int32 y = 0; y <= LastIndex; ++y)
+		{
+			const int32 Index = FMath::RandRange(y, LastIndex);
+			if (y != Index)
+			{
+				EnemySpawnPoints.Swap(y, Index);
+			}
+		}
+	}
+}
+
+void AEnemySpawnManager::SpawnEnemy()
+{
+	for (int32 i = 0; i < CurrentWaveData.MaxSpawnAtOnce; i++)
+	{
+		if (CurrentWaveData.MaxEnemies == CurrentWaveData.Spawned) break;
+		
+		RandomizeEnemies();
+		
+		for (AEnemySpawnPoint* SpawnPoint : EnemySpawnPoints)
+		{
+			if (SpawnPoint->Spawn(CurrentWaveData.Enemies[CurrentWaveData.Enemies.Num() - 1]))
+			{
+				CurrentWaveData.Spawned++;
+				CurrentWaveData.Enemies.RemoveAt(CurrentWaveData.Enemies.Num() - 1);
+				break;
+			}
+		}
+	}
+
+	FinishWave();
+}
+
+void AEnemySpawnManager::StartWave()
+{
+	GenerateWaveData();
+	GetWorldTimerManager().SetTimer(SpawnDelayTimer, this, &AEnemySpawnManager::SpawnEnemy, CurrentWaveData.SpawnDelay,
+	                                true);
+	OnWaveStarted.Broadcast();
+}
+
+void AEnemySpawnManager::FinishWave()
+{
+	if (CurrentWaveData.Spawned < CurrentWaveData.MaxEnemies)
+	{
+		return;
+	}
+
+	GetWorldTimerManager().ClearTimer(SpawnDelayTimer);
+	OnWaveFinished.Broadcast();
+}
+
+void AEnemySpawnManager::GenerateWaveData()
+{
+	TArray<TSubclassOf<AEnemy>> EnemyClasses;
+
+	TArray<FEnemyWaveData*> AllEnemies;
+	WaveTable->GetAllRows<FEnemyWaveData>("", AllEnemies);
+
+	for (const FEnemyWaveData* EnemyStat : AllEnemies)
+	{
+		for (int i = 0; i < EnemyStat->Count; i++)
+		{
+			EnemyClasses.Add(EnemyStat->Enemy);
+		}
+	}
+
+	const int32 LastIndex = EnemyClasses.Num() - 1;
+	for (int32 i = 0; i <= LastIndex; ++i)
+	{
+		const int32 Index = FMath::RandRange(i, LastIndex);
+
+		if (i != Index)
+		{
+			EnemyClasses.Swap(i, Index);
+		}
+	}
+
+	CurrentWaveData.Enemies = EnemyClasses;
+	CurrentWaveData.MaxEnemies = EnemyClasses.Num();
+	CurrentWaveData.Spawned = 0;
+	CurrentWaveData.SpawnDelay = SpawnDelay;
+	CurrentWaveData.MaxSpawnAtOnce = MaxSpawnAtOnce;
+}
---
+++ Source/LudumDare55/SpawnSystem/EnemySpawnManager.h
@@ -0,0 +1,106 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemySpawnManager.generated.h"
+
+class AEnemySpawnPoint;
+class AEnemy;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWaveStartedDynamicSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWaveFinishedDynamicSignature);
+
+USTRUCT(BlueprintType)
+struct FEnemyWaveData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy")
+	TSubclassOf<AEnemy> Enemy;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy", meta = (ClampMin = "1", UIMin = "1"))
+	int32 Weight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy", meta = (ClampMin = "1", UIMin = "1"))
+	int32 Count;
+};
+
+USTRUCT(BlueprintType)
+struct FWaveData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<TSubclassOf<AEnemy>> Enemies;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 MaxEnemies;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 Spawned;
+
+	UPROPERTY(BlueprintReadOnly)
+	float SpawnDelay;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 MaxSpawnAtOnce;
+};
+
+
+UCLASS()
+class LUDUMDARE55_API AEnemySpawnManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEnemySpawnManager();
+
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnWaveStartedDynamicSignature OnWaveStarted;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnWaveFinishedDynamicSignature OnWaveFinished;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UDataTable* WaveTable;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpawnDelay = 2.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int MaxSpawnAtOnce = 2;
+	
+protected:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsRandomSpawn = true;
+	
+	void RandomizeEnemies();
+
+	UFUNCTION()
+	void SpawnEnemy();
+
+	UFUNCTION(BlueprintCallable)
+	void StartWave();
+
+	UFUNCTION(BlueprintCallable)
+	void FinishWave();
+
+	void GenerateWaveData();
+	
+	
+private:	
+	TArray<AEnemySpawnPoint*> EnemySpawnPoints;
+
+	FWaveData CurrentWaveData;
+
+	UPROPERTY()
+	FTimerHandle SpawnDelayTimer;
+};

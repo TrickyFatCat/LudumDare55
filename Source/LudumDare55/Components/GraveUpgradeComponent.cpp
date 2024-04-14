57bf1fc75f2daf0d7ddb1f18d937c2bfafb792b5
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-15 00:27:19 +0300

    Fix project

---
+++ Source/LudumDare55/Components/GraveUpgradeComponent.cpp
@@ -0,0 +1,50 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "GraveUpgradeComponent.h"
+
+#include "MinionsCounterComponent.h"
+#include "SoulsCounterComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+UGraveUpgradeComponent::UGraveUpgradeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UGraveUpgradeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const APawn* Pawn = UGameplayStatics::GetPlayerPawn(this, 0);
+
+	SoulsCounterComponent = Pawn->GetComponentByClass<USoulsCounterComponent>();
+	MinionsCounterComponent = Pawn->GetComponentByClass<UMinionsCounterComponent>();
+
+	UpgradeCost = UpgradeTable.AsInteger(InitialLevel);
+
+	if (InitialLevel > 0)
+	{
+	}
+}
+
+bool UGraveUpgradeComponent::Upgrade()
+{
+	if (!SoulsCounterComponent || MinionsCounterComponent)
+	{
+		return false;
+	}
+
+	if (SoulsCounterComponent->GetSouls() < UpgradeCost)
+	{
+		return false;
+	}
+
+	SoulsCounterComponent->SpendSouls(UpgradeCost);
+	MinionsCounterComponent->IncreaseMaxNumber(MinionClass, 1);
+	Level += 1;
+	UpgradeCost = UpgradeTable.AsInteger(Level);
+	OnGraveUpgraded.Broadcast(MinionClass, Level, UpgradeCost, SoulsCounterComponent->GetSouls() < UpgradeCost);
+	return true;
+}
---
+++ Source/LudumDare55/Components/GraveUpgradeComponent.h
@@ -0,0 +1,60 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ScalableFloat.h"
+#include "Components/ActorComponent.h"
+#include "GraveUpgradeComponent.generated.h"
+
+
+class USoulsCounterComponent;
+class UMinionsCounterComponent;
+class AMinion;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnGraveUpgradedDynamicDelegate,
+                                              TSubclassOf<AMinion>, Class,
+                                              int32, NewLevel,
+                                              int32, NewUpgradeCost,
+                                              bool, bEnoughSouls);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE55_API UGraveUpgradeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UGraveUpgradeComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AMinion> MinionClass = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FScalableFloat UpgradeTable;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 UpgradeCost = 10;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 InitialLevel = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 Level = 0;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnGraveUpgradedDynamicDelegate OnGraveUpgraded;
+	
+	UFUNCTION(BlueprintCallable)
+	bool Upgrade();
+
+private:
+	UPROPERTY()
+	TObjectPtr<UMinionsCounterComponent> MinionsCounterComponent = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<USoulsCounterComponent> SoulsCounterComponent = nullptr;
+};

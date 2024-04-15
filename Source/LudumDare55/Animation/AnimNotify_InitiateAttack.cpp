d9af8a8834c7488f54658ecbe58bdd1b0c481bc0
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-15 23:11:19 +0300

    Create notify to initiate attack

---
+++ Source/LudumDare55/Animation/AnimNotify_InitiateAttack.cpp
@@ -0,0 +1,20 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "AnimNotify_InitiateAttack.h"
+
+#include "LudumDare55/Components/CharacterAttackComponent.h"
+
+
+void UAnimNotify_InitiateAttack::Notify(USkeletalMeshComponent* MeshComp,
+                                        UAnimSequenceBase* Animation,
+                                        const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	UCharacterAttackComponent* AttackComponent = MeshComp->GetOwner()->GetComponentByClass<UCharacterAttackComponent>();
+
+	if (AttackComponent)
+	{
+		AttackComponent->InitiateAttack();
+	}
+}

c182c4ba512d61eae2a65bfbaac242e2602d0017
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-15 22:51:23 +0300

    Create projectile actor class

---
+++ Source/LudumDare55/Characters/Attacks/Projectile.cpp
@@ -0,0 +1,55 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "Projectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+AProjectile::AProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	SetRootComponent(SphereComponent);
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+}
+
+void AProjectile::BeginPlay()
+{
+	ProjectileMovementComponent->Velocity = MovementDirection * ProjectileMovementComponent->InitialSpeed;
+	SphereComponent->IgnoreActorWhenMoving(GetOwner(), true);
+
+	SphereComponent->OnComponentHit.AddDynamic(this, &AProjectile::HandleProjectileHit);
+	
+	Super::BeginPlay();
+}
+
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AProjectile::SetProjectileData(const FVector& Direction, const int32 Damage)
+{
+	MovementDirection = Direction;
+	this->Damage = Damage <= 0 ? 1 : Damage;
+}
+
+void AProjectile::HandleProjectileHit(UPrimitiveComponent* HitComponent,
+                                      AActor* OtherActor,
+                                      UPrimitiveComponent* OtherComp,
+                                      FVector NormalImpulse,
+                                      const FHitResult& Hit)
+{
+	if (OtherActor)
+	{
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, nullptr, this, nullptr);
+	}
+
+	OnHit.Broadcast();
+	Destroy();
+}
---
+++ Source/LudumDare55/Characters/Attacks/Projectile.h
@@ -0,0 +1,49 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnHitSignature);
+
+UCLASS()
+class LUDUMDARE55_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnHitSignature OnHit;
+
+public:
+	auto SetProjectileData(const FVector& Direction, const int32 Damage) -> void;
+
+private:
+	FVector MovementDirection = FVector::ForwardVector;
+
+	int32 Damage = 1;
+
+	UFUNCTION()
+	void HandleProjectileHit(UPrimitiveComponent* HitComponent,
+	                         AActor* OtherActor,
+	                         UPrimitiveComponent* OtherComp,
+	                         FVector NormalImpulse,
+	                         const FHitResult& Hit);
+};

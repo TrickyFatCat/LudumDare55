31846ca72ca2c067bdf8bd54109db03666b9445b
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-04-15 13:26:06 +0300

    Create CharacterSpawnComponent

---
+++ Source/LudumDare55/Components/CharacterSpawnComponent.cpp
@@ -0,0 +1,38 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+
+#include "CharacterSpawnComponent.h"
+
+#include "GameFramework/Character.h"
+
+
+UCharacterSpawnComponent::UCharacterSpawnComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UCharacterSpawnComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	Character = Cast<ACharacter>(GetOwner());
+}
+
+void UCharacterSpawnComponent::StartSpawn()
+{
+	if (!Character || !AnimMontage)
+	{
+		FinishSpawn();
+	}
+	else
+	{
+		Character->PlayAnimMontage(AnimMontage);
+	}
+
+	OnSpawnStarted.Broadcast();
+}
+
+void UCharacterSpawnComponent::FinishSpawn()
+{
+	OnSpawnFinished.Broadcast();
+}
---
+++ Source/LudumDare55/Components/CharacterSpawnComponent.h
@@ -0,0 +1,46 @@
+﻿// Made by Titled Goose Team during Ludum Dare 55
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CharacterSpawnComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnSpawnStartedDynamicSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnSpawnFinishedDynamicSignature);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE55_API UCharacterSpawnComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UCharacterSpawnComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UAnimMontage* AnimMontage = nullptr;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnSpawnStartedDynamicSignature OnSpawnStarted;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnSpawnFinishedDynamicSignature OnSpawnFinished;
+	
+	UFUNCTION(BlueprintCallable)
+	void StartSpawn();
+	
+	UFUNCTION(BlueprintCallable)
+	void FinishSpawn();
+	
+private:
+	UPROPERTY()
+	ACharacter* Character = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<AAIController> AIController = nullptr;
+};
